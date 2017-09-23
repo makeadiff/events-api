29e2fc750372f5e87a7648ef011394ed462cfa9c
Binny V A <binnyva@gmail.com>
2017-09-23 16:59:22 +0530

    Forgot to include one file

---
+++ application/config/siteconfig.php
@@ -0,0 +1,7 @@
+<?php
+#Start - Rejeesh K.Nair
+$config['aa_report_choice'] = array("" => 'Select One',"1" => 'RSVP', "2" => 'Attendance');
+$config['aa_report_timeframe'] = array("1" => 'All',"2" => ' 1 Day', "3" => '1 Week', "4" => '2 Weeks', "5" => '4 Weeks');
+$config['output_format']	= 'html';
+#End - Rejeesh K.Nair
+?>
